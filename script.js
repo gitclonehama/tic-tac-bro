19d9d2fc9cafbba7cca926055ca2a645860a9415
Hama <pashazad@msu.edu>
2024-08-13 20:42:09 -0400

    Add game logic using factory function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello, World!");
+
+const Gameboard = (() => {
+  let board = ["", "", "",
+               "", "", "", 
+               "", "", ""];
+  
+  const getBoard = () => board;
+
+  const setBoard = (index, marker) => {
+    if (board[index] === "") {
+      board[index] = marker;
+      return true;
+    }
+    else {
+      alert("Spot taken!")
+      return false;
+    }
+  };
+
+  return {getBoard, setBoard};
+})();
